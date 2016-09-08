933f528c3fa866fad562d664aba3378a715abfd3
Alejandro Magual <alejandro.magual@yahoo.ca>
2016-09-08 17:23:45 +0200

    Canvas improved

--- wk2/Canvas/script.js
+++ wk2/Canvas/script.js
@@ -1,16 +1,17 @@
 var context = document.getElementById('canv').getContext('2d');
 
-context.strokeStyle = '#900';
+context.strokeStyle = '#000';
+context.lineWidth = 5;
 context.beginPath();
-context.arc(300,220,80,0,2*Math.PI);
+context.arc(300,230,70,0,2*Math.PI);
 context.moveTo(300,300);
 context.lineTo(300,500);
-context.moveTo(300,400);
-context.lineTo(500,300);
-context.moveTo(300,400);
-context.lineTo(100,300);
+context.moveTo(300,370);
+context.lineTo(450,290);
+context.moveTo(300,370);
+context.lineTo(150,290);
 context.moveTo(300,500);
-context.lineTo(450,650);
+context.lineTo(420,620);
 context.moveTo(300,500);
-context.lineTo(150,650);
+context.lineTo(180,620);
 context.stroke();
