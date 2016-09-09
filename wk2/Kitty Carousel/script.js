1530ebcc2b72d6aff9ebeded87ef68c2f307d4e9
Alejandro Magual <alejandro.magual@yahoo.ca>
2016-09-09 15:34:24 +0200

    Kitty Carousel work in progress

--- wk2/Kitty Carousel/script.js
+++ wk2/Kitty Carousel/script.js
@@ -1,36 +1,28 @@
 
+// Create "element" Node List with four kitty images
 var element = document.getElementsByClassName('kittyImages');
-var i = 3;
 
-element[(i-2) % 4].style.transform = 'translateX(-100%)';
-element[(i-1) % 4].style.transform = 'translateX(-100%)';
-element[(i+1) % 4].style.transform = 'translateX(100%)';
-setTimeout(carousel,5000);
+var i = 0;  // initialize index to loop through "element" node list
 
+// Initial element[] position: [0] on-screen ; [1]-[3] off-screen (right)
+element[1].style.transform = 'translateX(100%)';
+element[2].style.transform = 'translateX(100%)';
+element[3].style.transform = 'translateX(100%)';
+
+// "carousel" function executes animation effect (recursive; setTimeout callback every 5s)
+setTimeout(carousel,3000);
 
 function carousel() {
-    element[(i-1) % 4].style.transform = 'translateX(0%)';
-    element[(i % 4)].style.transform = 'translateX(100%)';
-    element[(i+1) % 4].style.transform = 'translateX(-100%)';
+    // Before sliding animation starts, add event 'transitionend' to element[i%4]
+    // to move it off-screen (right) once sliding effect is completed
+    element[i%4].addEventListener('transitionend',test);
+    function test() {
+        this.style.transitionDuration = '0s';
+        this.style.transform = "translateX(100%)";
+    }
+    // sliding animation effect and loop
+    element[i%4].style.transform = 'translateX(-100%)';
+    element[(i+1)%4].style.transform = 'translateX(0%)';
     i++;
-    setTimeout(carousel,5000);
+    setTimeout(carousel,3000);
 }
-
-
-/* TEST:
-var element = document.getElementsByClassName('kittyImages');
-var i = 3;
-setTimeout(test,2000);
-
-function carousel() {
-    element[i].style.transform = 'translateX(100%)';
-    if (i >= 0) {i--;};
-    setTimeout(carousel,5000);
-}
-
-function test() {
-    element[i].style.display = 'none';
-    if (i >= 0) {i--;};
-    setTimeout(test,2000);
-}
-*/
